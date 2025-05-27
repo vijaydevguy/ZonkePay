ec9d0d4172681695b2533d2b39a75b86c3c7fb18
vijaydevguy <vijayakumarjob01@gmail.com>
2025-05-27 15:41:42 +0530

    error fixes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+//   ignoreBuildErrors: true,
+
+// eslint: {
+//     ignoreDuringBuilds: true
+//   },
+
+output: 'export',
+images: {
+  unoptimized: true,
+},
 };
 
 export default nextConfig;
+
+
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
